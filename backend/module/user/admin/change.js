19f3509b1c4e730a4d82ba539df8303b42c9b951
Domi <mmillion448@gmail.com>
2024-02-01 21:08:41 -0500

    refactored user/admin/change.js

--- backend/module/user/admin/change.js
+++ backend/module/user/admin/change.js
@@ -1,31 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const client = require("../../../setup/db");
 const helpers = require("../../../utilities/helpers");
+const User = require('../../../models/user');
 
-router.put("/", (req, res) => {
+router.put("/", async (req, res) => {
     if (!("username" in req.body) || helpers.name_validate(req.body["username"])) {
-        res.status(400).json({ message: "The username is missing or has invalid format." });
-        return;
+        return res.status(400).json({ message: "The username is missing or has invalid format." });
     }
     if (!("email" in req.body) || helpers.email_validate(req.body["email"])) {
-        res.status(400).json({ message: "The email is missing or has invalid format." });
-        return;
+        return res.status(400).json({ message: "The email is missing or has invalid format." });
     }
 
-    let sql_update = "UPDATE user_info SET email = ($1) WHERE username = ($2)";
-    let sql_update_data = [req.body["email"], req.body["username"].toLowerCase()];
+    const { username, email } = req.body;
 
-    client.query(sql_update, sql_update_data, (err, pg_res) => {
-        if (err) {
-            res.status(404).json({ message: "Unknown error." });
-            console.log(err);
-        } else if (pg_res.rowCount === 0) {
-            res.status(400).json({ message: "The username is invalid." });
-        } else {
-            res.status(200).json({ message: "The user info is changed." });
-        }
-    });
+    const user = await User.findOne({ where: { username }});
+
+    if (!user) return res.status(400).json({ message: "The username is invalid." });
+
+    user.set({ email });
+    await user.save();
+
+    const userObj = {
+        email: user.email,
+        username: user.username,
+    };
+
+    return res
+        .status(200)
+        .json({ message: "The user info is changed.", user: userObj });
 })
 
 module.exports = router;
