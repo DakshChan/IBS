66ca2ad121ab78af6f5e6cceaee5ab3d170b8f4a
Domi <mmillion448@gmail.com>
2024-02-06 14:14:45 -0500

    moved ROLES constants to global constants file

--- backend/tests/utils/constants.js
+++ backend/tests/utils/constants.js
@@ -1,9 +1,3 @@
 module.exports = {
     BASE_API_URL: "http://localhost:3001",
-    ROLES: {
-        instructor: 'instructor',
-        teachingAssistant: 'ta',
-        student: 'student',
-        admin: 'admin'
-    }
 }
