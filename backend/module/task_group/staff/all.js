cc47113c83ecae28d731d1a35f998eddfaab835d
Domi <mmillion448@gmail.com>
2024-02-11 21:25:24 -0500

    temp modification of GET all task_groups endpoints

--- backend/module/task_group/staff/all.js
+++ backend/module/task_group/staff/all.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const client = require("../../../setup/db");
 
 router.get("/", (req, res) => {
-    let sql_task_group = "SELECT * FROM course_" + res.locals["course_id"] + ".task_group ORDER BY task_group_id";
+    let sql_task_group = "SELECT * FROM task_groups WHERE course_id=" + res.locals["course_id"] + " ORDER BY task_group_id";
     client.query(sql_task_group, [], (err, pg_res) => {
         if (err) {
             res.status(404).json({ message: "Unknown error." });
