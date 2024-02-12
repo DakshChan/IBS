1b836e6799d57a5e900a610452e35f716335f1c1
Domi <mmillion448@gmail.com>
2024-02-11 23:26:19 -0500

    refactored get all task groups endpoint

--- backend/module/task_group/staff/all.js
+++ backend/module/task_group/staff/all.js
@@ -2,15 +2,14 @@ const express = require("express");
 const router = express.Router();
 const client = require("../../../setup/db");
 
-router.get("/", (req, res) => {
-    let sql_task_group = "SELECT * FROM task_groups WHERE course_id=" + res.locals["course_id"] + " ORDER BY task_group_id";
-    client.query(sql_task_group, [], (err, pg_res) => {
-        if (err) {
-            res.status(404).json({ message: "Unknown error." });
-        } else {
-            res.status(200).json({ count: pg_res.rowCount, task_group: pg_res.rows });
-        }
-    });
+const TaskGroup = require("../../../models/taskgroup");
+
+router.get("/", async (req, res) => {
+    const { course_id }  = res.locals;
+
+    const task_groups = await TaskGroup.findAll({ where: { course_id } });
+
+    return res.status(200).json({ count: task_groups.length, task_group: task_groups });
 })
 
 module.exports = router;
