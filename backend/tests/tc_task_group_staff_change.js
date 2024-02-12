c9fe3493ad63f3b421fa8bc94449a2e38c625aad
Domi <mmillion448@gmail.com>
2024-02-12 12:35:43 -0500

    created tests and seeder for PUT change task_group endpoint

---
+++ backend/tests/tc_task_group_staff_change.js
@@ -0,0 +1,68 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require("../app"); // Adjust the path as per your project structure
+const sequelize = require('../helpers/database');
+const { getAuthBearerToken } = require("./utils/helpers");
+const { BASE_API_URL } = require("./utils/constants"); // Adjust the path as per your project structure
+const { ROLES } = require("../helpers/constants")
+
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+/**
+ * Constructs the endpoint to modify a task group as the given role for the
+ * course with given course id
+ * @param role one of the values in ./constants.js ROLES
+ * @param course_id the id of the course
+ * @returns {`/${string}/course/${string}/task_group/change`}
+ */
+const modifyTaskGroupEndpoint = (role, course_id) => {
+    return `/${role}/course/${course_id}/task_group/change`;
+};
+
+describe('[task_group/staff module]: PUT modify task group endpoint', () => {
+    let matInstructorToken, cscInstructorToken, cscStudentToken;
+
+    before(async () => {
+        cscInstructorToken = await getAuthBearerToken('cscinstructoruser', 'password');
+        cscStudentToken = await getAuthBearerToken('cscstudentuser', 'password');
+
+        matInstructorToken = await getAuthBearerToken('matinstructoruser', 'password');
+    });
+
+    it('instructor should be be able to add a task group', (done) => {
+        chai.request(BASE_API_URL)
+            .put(modifyTaskGroupEndpoint(ROLES.instructor, 1))
+            .set('Authorization', cscInstructorToken)
+            .send({ max_token: 15, task_group_id: 1 })
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('student cannot modify task group tokens', (done) => {
+        chai.request(BASE_API_URL)
+            .put(modifyTaskGroupEndpoint(ROLES.instructor, 1))
+            .set('Authorization', cscStudentToken)
+            .send({ max_token: 100, task_group_id: 1 })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
+    });
+
+    it('instructor cannot modify task group tokens for a different course', (done) => {
+        chai.request(BASE_API_URL)
+            .put(modifyTaskGroupEndpoint(ROLES.instructor, 1))
+            .set('Authorization', matInstructorToken)
+            .send({ max_token: 2, task_group_id: 1 })
+            .end((err, res) => {
+                expect(res).to.have.status(400);
+                done();
+            });
+    });
+
+    after(async () => {
+    });
+});
