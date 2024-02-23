f5c8fe80d1fd8d1ab4e214e576fa9dbfe1d6c096
Andrew Qiu <qiuandrew@live.com>
2024-02-22 20:21:07 -0500

    Removed unnecessary comments.

--- backend/module/task/student/all.js
+++ backend/module/task/student/all.js
@@ -4,15 +4,6 @@ const client = require("../../../setup/db");
 const {Task} = require("../../../models");
 
 router.get("/", async (req, res) => {
-    // let sql_task = "SELECT task, long_name, to_char(due_date AT TIME ZONE 'America/Toronto', 'YYYY-MM-DD HH24:MI:SS') AS due_date, due_date AS due_date_utc, hidden, weight, min_member, max_member, max_token, change_group, hide_interview, hide_file, interview_group, task_group_id, starter_code_url FROM course_" + res.locals["course_id"] + ".task WHERE hidden = 'false' ORDER BY due_date, task";
-    // client.query(sql_task, [], (err, pg_res) => {
-    //     if (err) {
-    //         res.status(404).json({ message: "Unknown error." });
-    //     } else {
-    //         res.status(200).json({ count: pg_res.rowCount, task: pg_res.rows });
-    //     }
-    // });
-
     const course_id = res.locals["course_id"];
 
     try {
