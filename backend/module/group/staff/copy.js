e4d86da7ed58c190a005e0fb5062223443c15740
Domi <mmillion448@gmail.com>
2024-04-05 06:17:51 -0400

    refactored group/staff/copy.js

--- backend/module/group/staff/copy.js
+++ backend/module/group/staff/copy.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const helpers = require("../../../utilities/helpers");
 
 router.post("/", (req, res) => {
-	if (!("from_task" in req.body) || helpers.name_validate(req.body["from_task"])) {
-		res.status(400).json({ message: "The from task is missing or has invalid format." });
-		return;
+	if (!req.body.from_task || helpers.name_validate(req.body.from_task)) {
+		return res.status(400).json({ message: "The from task is missing or has invalid format." });
 	}
-	if (!("to_task" in req.body) || helpers.name_validate(req.body["to_task"])) {
-		res.status(400).json({ message: "The to task is missing or has invalid format." });
-		return;
+
+	if (!req.body.to_task || helpers.name_validate(req.body.to_task)) {
+		return res.status(400).json({ message: "The to task is missing or has invalid format." });
 	}
 
 	helpers.copy_groups(res.locals["course_id"], req.body["from_task"], req.body["to_task"]).then(results => {
